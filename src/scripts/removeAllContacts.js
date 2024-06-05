14a444245a75142c7f2e41b88ebd3d1505d19667
Andromeda-NGC224 <shadim115115@gmail.com>
2024-06-05 17:27:10 +0300

    Done 7 steps

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,5 +1,14 @@
-import { PATH_DB } from '../constants/contacts.js';
+import { PATH_DB } from '../constants/contacts.js'
+import fs from 'fs/promises'
 
-export const removeAllContacts = async () => {};
+export const removeAllContacts = async () => {
+  try {
+    const clearList = []
 
-await removeAllContacts();
+    await fs.writeFile(PATH_DB, JSON.stringify(clearList))
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+await removeAllContacts()
